a1bc8e590d8af08478e9debd761385867b711ff9
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-08-01 15:26:39 -0300

    l

--- cats.controller.js
+++ cats.controller.js
@@ -1,5 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 
+const GATOS = [
+  {
+    id: 1,
+    nome: "Franeudos",
+    corOlhos: "verde",
+    raça: "sphynx"
+  }
+]
+
 @Controller('cats')
 export class CatsController {
   @Get()
